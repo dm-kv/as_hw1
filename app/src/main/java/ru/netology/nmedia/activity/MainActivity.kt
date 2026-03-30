74aaab1dec0d9188a6304739e6fb48dda3a8b7d0
Dm Kv <demon1st@yandex.ru>
2026-03-31 01:45:26 +0300

    task 7

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -88,6 +88,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        
         binding.cancel.setOnClickListener {
             binding.content.setText("")
             binding.content.clearFocus()
