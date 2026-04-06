9f85be2590085133a43854576a433281926b15a3
Dm Kv <demon1st@yandex.ru>
2026-04-06 10:37:17 +0300

    task 9

--- app/src/main/java/ru/netology/nmedia/activity/NewPostActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/NewPostActivity.kt
@@ -49,17 +49,15 @@ object NewPostContract: ActivityResultContract<Unit, String?>() {
     ) = intent?.getStringExtra(Intent.EXTRA_TEXT)
 }
 
-object EditPostContract : ActivityResultContract<String, String?>() {
-
+object EditPostContract : ActivityResultContract<String?, String?>() {
 
     override fun createIntent(
         context: Context,
-        input: String
-    ): Intent =
-        Intent(context, NewPostActivity::class.java).apply {
+        input: String?
+    ) = Intent(context, NewPostActivity::class.java).apply {
             putExtra(Intent.EXTRA_TEXT, input)
         }
-
+    
     override fun parseResult(resultCode: Int, intent: Intent?): String? =
         if (resultCode == Activity.RESULT_OK) {
             intent?.getStringExtra(Intent.EXTRA_TEXT)
